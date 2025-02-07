47e42cc5678f4513bc7e1820728e09baecc1bb56
amiltonxavier <amilton.jose@etic.co.ao>
2025-02-07 17:40:45 +0100

    header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,29 +1,52 @@
 import { Clapperboard, LogOut, Popcorn } from "lucide-react";
+import { useState } from "react";
+
+const tabsApp = [
+    {
+        name: 'Explorar',
+        id: 'explore'
+    },
+    {
+        name: 'Meus filmes',
+        id: 'my-movies'
+    }
+]
 
 export function Header() {
+    const [tab, setTab] = useState(tabsApp[0].id);
+    function handleActiveTab(id: string) {
+        setTab(id)
+    }
     return (
-        <div>
+        <div className="flex justify-between items-center border-b py-5 px-6">
             <button>
-                <img src="/Logo.svg" alt="logo" className="object-cover w-full " />
+                <img src="/Logo.svg" alt="logo" className="object-cover h-12 w-12" />
             </button>
 
-            <div>
-                <button type="button">
-                    <Popcorn />
-                    Explorar
-                </button>
-                <button type="button">
-                    <Clapperboard />
-                    Meus filmes
-                </button>
+            <div className="flex items-center gap-6">
+
+                {
+                    tabsApp.map((item) => (
+                        <button type="button" key={item.id}
+                            className={`flex items-center gap-2 cursor-pointer py-2 px-3 rounded-lg ${tab === item.id ? 'text-[#892CCD] bg-[#1A1B2D]' : 'text-[#7A7B9F]'} `}
+                            onClick={() => handleActiveTab(item.id)}
+                        >
+                            {
+                                item.id === 'explore' ? <Popcorn /> : <Clapperboard />
+                            }
+                            {item.name}
+                        </button>
+                    ))
+                }
+
             </div>
 
-            <div>
+            <div className="flex items-center gap-2">
                 <span>Olá, Jordan</span>
-                <div />
+                <div className="h-full w-1 bg-gray-300" />
 
-                <button type="button">
-                    <LogOut />
+                <button type="button" className="bg-[#1A1B2D] p-1.5 rounded-lg ">
+                    <LogOut className="size-5" />
                 </button>
             </div>
         </div>
